05d3144c322374a4f3df18bbc2d4ccd3dd08130f
Paweł Bubak <pawelbubak97@gmail.com>
2019-03-01 09:13:37 +0100

    Draft of the Radon transform implementation

--- radon-transform.py
+++ radon-transform.py
@@ -1,7 +1,25 @@
-class RadonTransform:
-    def transform(self):
-        print('Transform')
+from point import Point
+from bresenham import Bresenham
+from section import Section
 
+class RadonTransform:
+    def transform(self, image, emiter_position, detector_positions):
+        image_points = []
+        sections = self.__get_section(emiter_position, detector_positions)
+        for section in sections:
+            image_points = Bresenham().get_points(section.start_point, section.end_point)
+            image_points = self.__filter_image_points()
+        return image_points
+
+    def __get_sections(self, emiter_position, detector_positions):
+        sections = []
+        for position in detector_positions:
+            sections.append(Section(emiter_position, position))
+        return sections
+    
+    def __filter_image_points(image_height, image_width, points):
+        return points
+    
     def inverse_transform(self):
         print('InverseTransform')
 
