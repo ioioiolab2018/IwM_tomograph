d6fd7c2c7bd9c6b3db9b8694da8bb59e073e5828
pawelbubak <pawelbubak97@gmail.com>
2019-03-02 18:21:33 +0100

    Poprawka testu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bresenham import Bresenham
 from point import Point
 from radon_transform import RadonTransform
+from inverse_radon_transform import InverseRadonTransform
+from convolution import Convolution
 from tomograph import Tomograph
 import matplotlib.pyplot as plt
 from skimage import color
@@ -27,18 +29,19 @@ n = 400
 alpha = 0.7
 tomograph = Tomograph(alpha, n, 90, arr.shape[0], arr.shape[1])
 
-x = []
-y = []
-for i in range(0, 360):
-    for d in range(0, 3):
-        wynik = tomograph.get_ray(i, d)
-        x.append(wynik.end_point.x)
-        y.append(wynik.end_point.y)
-
-plt.plot(x, y, 'ro')
-plt.show()
+# x = []
+# y = []
+# for i in range(0, 360):
+#     for d in range(0, 3):
+#         wynik = tomograph.get_ray(i, d)
+#         x.append(wynik.end_point.x)
+#         y.append(wynik.end_point.y)
+#
+# plt.plot(x, y, 'ro')
+# plt.show()
 
 sinogram = RadonTransform().transform(arr, tomograph)
+sinogram = Convolution().transform(sinogram, [-1, 3, -1])
 
 arr2 = np.asarray(sinogram)
 arr2 = np.transpose(arr2)
@@ -46,7 +49,7 @@ image = resize(arr2, (100, 200))
 plt.imshow(image, cmap='gray')
 plt.show()
 
-result = RadonTransform().inverse_transform(sinogram, tomograph)
+result = InverseRadonTransform().transform(sinogram, tomograph)
 plt.imshow(result, cmap='gray')
 plt.show()
 
