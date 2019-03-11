e55b80cae79acba8a09fb3128c7242ee52c163d8
pawelbubak <pawelbubak97@gmail.com>
2019-03-11 19:42:36 +0100

    Usunięcie zbędnych komentarzy

--- radon_transform.py
+++ radon_transform.py
@@ -3,7 +3,6 @@ from section import Section
 from point import Point
 from tomograph import Tomograph
 import math
-import time
 
 
 class RadonTransform:
@@ -11,7 +10,6 @@ class RadonTransform:
         iteration_no = math.floor(360 / tomograph.dAlpha)
         sinogram = []
         for position_number in range(0, iteration_no):
-            print(position_number)
             sinogram_vector = self.get_singoram_vector(image, tomograph, position_number)
             sinogram.append(sinogram_vector)
         return sinogram
@@ -19,27 +17,15 @@ class RadonTransform:
     def get_singoram_vector(self, image, tomograph: Tomograph, position_number):
         sinogram_vector = []
         for j in range(0, tomograph.n):
-            # start1 = time.clock()
             section = tomograph.get_ray(position_number, j)
-            # end1 = time.clock()
-            # start2 = time.clock()
             sinogram_vector.append(self.get_sinogram_value(image, section))
-            # end2 = time.clock()
-            # print('CALCULATE_RAY_TIME: ' + str(end1 - start1))
-            # print('CALCULATE_VALUE_TIME: ' + str(end2 - start2))
         return sinogram_vector
 
     def get_sinogram_value(self, image, section: Section):
         image_height = len(image)
         image_width = len(image[0])
-        # start1 = time.clock()
         image_points = Bresenham().get_image_points(section.start_point, section.end_point, image_width, image_height)
-        # end1 = time.clock()
-        # start2 = time.clock()
         value = self.__calculate_sinogram_value(image, image_points)
-        # end2 = time.clock()
-        # print('BRESENHAM_TIME: ' + str(end1 - start1))
-        # print('CALCULATE_VALUE_TIME: ' + str(end2 - start2))
         return value
 
     def __calculate_sinogram_value(self, image, points: [Point]):
--- tomograph.py
+++ tomograph.py
@@ -12,13 +12,17 @@ class Tomograph:
 
     def get_ray(self, iteration, detector_no):
         r = sqrt(self.height ** 2 + self.width ** 2) / 2
+        # print('RADIUS: ' + str(r))
         emiter = Point(r * sin(radians(iteration*self.dAlpha)),
                        r * cos(radians(iteration*self.dAlpha)))
+        # print('EMITER:' + '\n\tX: ' + str(emiter.x) + '\n\tY: ' + str(emiter.y))
         opposite_point = Point(-emiter.x, -emiter.y)
+        # print('OPOSITE_POINT:' + '\n\tX: ' + str(opposite_point.x) + '\n\tY: ' + str(opposite_point.y))
         alpha = radians(self.arcLenght) / 2 - detector_no * (radians(self.arcLenght) / (self.n - 1))
         detector_x = opposite_point.x * cos(alpha) - opposite_point.y * sin(alpha)
         detector_y = opposite_point.x * sin(alpha) + opposite_point.y * cos(alpha)
         detector = Point(floor(detector_x+self.width/2), floor(detector_y+self.height/2))
+        # print('DETECTOR:' + '\n\tX: ' + str(detector_x) + '\n\tY: ' + str(detector_y))
         emiter.x = floor(emiter.x+self.width/2)
         emiter.y = floor(emiter.y+self.height/2)
         return Section(emiter, detector)
