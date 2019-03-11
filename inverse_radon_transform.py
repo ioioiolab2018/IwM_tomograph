985ff9afc9b42185919a59be3b9bf2e8709e6403
pawelbubak <pawelbubak97@gmail.com>
2019-03-11 19:43:35 +0100

    Refaktoryzacja algorytmu odwrotnej transformaty Radona

--- inverse_radon_transform.py
+++ inverse_radon_transform.py
@@ -1,31 +1,40 @@
 from bresenham import Bresenham
 from tomograph import Tomograph
+from point import Point
 import numpy as np
 
 
 class InverseRadonTransform:
     def transform(self, sinogram, tomograph: Tomograph):
-        size = (tomograph.width, tomograph.height)
-        iteration = len(sinogram)
-        detectors_no = len(sinogram[0])
-        print(iteration)
-        print("aaaaaaaaaa")
-        print(detectors_no)
-        image = np.zeros(size)
-        counter = np.zeros(size)
-        for i in range(0, iteration):
-            for d in range(0, detectors_no):
-                section = tomograph.get_ray(i, d)
-                image_points = Bresenham().get_image_points(section.start_point, section.end_point, tomograph.width,
-                                                            tomograph.height)
-                # image_points = self.__filter_image_points(size[1], size[0], image_points)
-                for point in image_points:
-                    if point.x < size[0] and point.y < size[1]:
-                        image[point.x][point.y] += sinogram[i][d]
-                        counter[point.x][point.y] += 1
-        for i in range(0, size[0]):
-            for j in range(0, size[1]):
-                if counter[i][j] > 0:
-                    image[i][j] = image[i][j] / counter[i][j]
+        image_size = (tomograph.width, tomograph.height)
+        image = np.zeros(image_size)
+        lines_quantity_per_pixel = np.zeros(image_size)
 
+        rays_quantity = len(sinogram)
+        detectors_quantity = len(sinogram[0])
+        for vector_number in range(0, rays_quantity):
+            self.__transform_vector(image, lines_quantity_per_pixel, detectors_quantity, sinogram, tomograph,
+                                    vector_number)
+        self.normalize_image(image, lines_quantity_per_pixel)
         return image
+
+    def __transform_vector(self, image: [[float]], lines_quantity_per_pixel: [[int]], detectors_quantity: int, sinogram,
+                           tomograph: Tomograph, vector_number: int):
+        for detector_number in range(0, detectors_quantity):
+            section = tomograph.get_ray(vector_number, detector_number)
+            image_points = Bresenham().get_image_points(section.start_point, section.end_point, tomograph.width,
+                                                        tomograph.height)
+            self.__transform_value(image, lines_quantity_per_pixel, image_points,
+                                   sinogram[vector_number][detector_number])
+
+    def normalize_image(self, image: [[float]], lines_quantity_per_pixel: [[int]]):
+        for i in range(0, len(lines_quantity_per_pixel)):
+            for j in range(0, len(lines_quantity_per_pixel[0])):
+                if lines_quantity_per_pixel[i][j] > 0:
+                    image[i][j] = image[i][j] / lines_quantity_per_pixel[i][j]
+
+    def __transform_value(self, image: [[float]], lines_quantity_per_pixel: [[int]], image_points: [Point],
+                          sinogram_value: float):
+        for point in image_points:
+            image[point.x][point.y] += sinogram_value
+            lines_quantity_per_pixel[point.x][point.y] += 1
