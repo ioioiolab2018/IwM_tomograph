802b523fdf6ffac80b9b4354c4090b49a63dcd21
pawelbubak <pawelbubak97@gmail.com>
2019-03-12 18:33:49 +0100

    Refaktoryzacja odwrotnej transformaty Radona

--- inverse_radon_transform.py
+++ inverse_radon_transform.py
@@ -5,20 +5,20 @@ import numpy as np
 
 
 class InverseRadonTransform:
-    def transform(self, sinogram, tomograph: Tomograph):
+    def transform(self, sinogram: [[float]], tomograph: Tomograph):
         image_size = (tomograph.image_width, tomograph.image_height)
         image = np.zeros(image_size)
         lines_quantity_per_pixel = np.zeros(image_size)
 
         rays_quantity = len(sinogram)
         detectors_quantity = len(sinogram[0])
-        for vector_number in range(0, rays_quantity):
+        for sinogram_vector_number in range(0, rays_quantity):
             self.__transform_vector(image, lines_quantity_per_pixel, detectors_quantity, sinogram, tomograph,
-                                    vector_number)
+                                    sinogram_vector_number)
         return self.normalize_image(image, lines_quantity_per_pixel)
 
-    def __transform_vector(self, image: [[float]], lines_quantity_per_pixel: [[int]], detectors_quantity: int, sinogram,
-                           tomograph: Tomograph, vector_number: int):
+    def __transform_vector(self, image: [[float]], lines_quantity_per_pixel: [[int]], detectors_quantity: int,
+                           sinogram: [[float]], tomograph: Tomograph, vector_number: int):
         for detector_number in range(0, detectors_quantity):
             section = tomograph.get_ray(vector_number, detector_number)
             image_points = Bresenham().get_image_points(section.start_point, section.end_point, tomograph.image_width,
@@ -40,14 +40,14 @@ class InverseRadonTransform:
             image[point.x][point.y] += sinogram_value
             lines_quantity_per_pixel[point.x][point.y] += 1
 
-    def get_partial_result(self, sinogram, tomograph: Tomograph, progress, image=None, counter=None, start=0):
-        size = (tomograph.image_width, tomograph.image_height)
-        detectors_no = len(sinogram[0])
+    def get_partial_result(self, sinogram: [[float]], tomograph: Tomograph, progress: int, image: [[float]] = None,
+                           lines_quantity_per_pixel: [[int]] = None, start: int = 0):
+        image_size = (tomograph.image_width, tomograph.image_height)
+        if image is None or lines_quantity_per_pixel is None:
+            image = np.zeros(image_size)
+            lines_quantity_per_pixel = np.zeros(image_size)
+        detectors_quantity = len(sinogram[0])
 
-        if image is None or counter is None:
-            image = np.zeros(size)
-            counter = np.zeros(size)
         for iteration in range(start, progress):
-            self.__transform_vector(image, counter, detectors_no, sinogram, tomograph, iteration)
-
-        return image, counter
+            self.__transform_vector(image, lines_quantity_per_pixel, detectors_quantity, sinogram, tomograph, iteration)
+        return image, lines_quantity_per_pixel
